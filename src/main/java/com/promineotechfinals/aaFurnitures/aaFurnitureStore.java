5f79288399224d43f2f3c4bb66d1826d0205f5bb
AshaAbdu <halge87@yahoo.com>
2022-11-05 23:58:06 -0500

    web application with tomcat

---
+++ src/main/java/com/promineotechfinals/aaFurnitures/aaFurnitureStore.java
@@ -0,0 +1,14 @@
+package com.promineotechfinals.aaFurnitures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class aaFurnitureStore {
+
+	public static void main(String[] args) {
+		SpringApplication.run(aaFurnitureStore.class, args);
+
+	}
+
+}
