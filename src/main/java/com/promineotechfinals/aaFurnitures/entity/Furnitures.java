b4cceed4ad1a5c8e5b23e927d990b82d0afaff4f
AshaAbdu <halge87@yahoo.com>
2022-11-13 15:11:01 -0600

    fixing and breaing test for component level

--- src/main/java/com/promineotechfinals/aaFurnitures/entity/Furnitures.java
+++ src/main/java/com/promineotechfinals/aaFurnitures/entity/Furnitures.java
@@ -1,5 +1,21 @@
 package com.promineotechfinals.aaFurnitures.entity;
 
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder 
+@NoArgsConstructor
+@AllArgsConstructor
 public class Furnitures {
+	
+	private Long roomPK;
+	private FurnitureRoom roomId;
+	private String material;
+	private BigDecimal price;
 
 }
--- src/test/java/com/promineotechfinals/aaFurnitures/controller/support/FetchFurnitureTestSupport.java
+++ src/test/java/com/promineotechfinals/aaFurnitures/controller/support/FetchFurnitureTestSupport.java
@@ -1,13 +1,34 @@
 package com.promineotechfinals.aaFurnitures.controller.support;
 
+import java.math.BigDecimal;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+
+import com.promineotechfinals.aaFurnitures.entity.FurnitureRoom;
 import com.promineotechfinals.aaFurnitures.entity.Furnitures;
 
 public class FetchFurnitureTestSupport extends BaseTest {
 	protected List<Furnitures> buildExpected() {
 		List<Furnitures> list = new LinkedList<>();
 		
+		// @formatter: off
+		
+		list.add(Furnitures.builder()
+				.roomId(FurnitureRoom.BED_ROOM)
+				.material("Wood")
+				.price(new BigDecimal("1485.00"))
+				.build());
+		
+		list.add(Furnitures.builder()
+				.roomId(FurnitureRoom.BED_ROOM)
+				.material("Fabric")
+				.price(new BigDecimal("1225.00"))
+				.build());
+		
+		// @formatter: on
+		
+		//Collections.sort(list);
 		return list;
 	}
 }
